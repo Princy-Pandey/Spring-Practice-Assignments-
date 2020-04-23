401533e62f43fcbc1e865f2b781a12cccdf0ac89
Princy Pandey <60910695+Princy-Pandey@users.noreply.github.com>
2020-04-24 00:52:24 +0530

    Add files via upload

---
+++ SpringBootDemo/src/main/java/com/cg/springboot/Demo1.java
@@ -0,0 +1,15 @@
+package com.cg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Demo1.class, args);
+
+	}
+
+} 
